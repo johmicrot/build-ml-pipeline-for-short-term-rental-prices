2fad42662a81d2d5341fae826817d7b6514f4785
John Rothman <johns.roth@gmail.com>
2024-11-05 16:24:30 +0100

    update random forest trainer

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -89,16 +89,15 @@ def go(args):
     logger.info("Exporting model")
 
     # Save model package in the MLFlow sklearn format
-    if os.path.exists("random_forest_dir"):
-        shutil.rmtree("random_forest_dir")
+    if os.path.exists("random_forest"):
+        shutil.rmtree("random_forest")
 
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
     mlflow.sklearn.save_model(
         sk_pipe,
-        "random_forest_dir",
-        signature=infer_signature(X_val, y_pred),
+        "random_forest",
         serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
         input_example=X_val.head()
     )
@@ -116,7 +115,7 @@ def go(args):
         description="Trained pipeline artifact",
         metadata=rf_config
     )
-    artifact.add_dir(export_path)
+    artifact.add_dir('random_forest')
     run.log_artifact(artifact)
     ######################################
 
@@ -169,7 +168,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # Build a pipeline with two steps:
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
-    non_ordinal_categorical_preproc = make_pipeline( SimpleImputer(),OneHotEncoder() )
+    non_ordinal_categorical_preproc = make_pipeline( SimpleImputer(strategy="most_frequent"),OneHotEncoder() )
     ######################################
 
     # Let's impute the numerical columns to make sure we can handle missing values
